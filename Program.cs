f70b2cbf1e3232eccdf89688e7cfaa62e2ce7715
Luis Jesús Díaz Manzo <luis@drconopoima.com>
2023-11-03 12:56:30 +0100

    Enum CurrencyCode

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace CurrencyConverterDotNet;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        CurrencyCode code = CurrencyCode.USD;
+        Console.WriteLine($"{code.GetCode()}");
+    }
+}
